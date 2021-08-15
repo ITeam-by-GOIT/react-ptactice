7c05c026b936430d527e0173f1d10f2c0dc1d52c
Anastasia <nastasia199126@gmail.com>
2021-08-15 10:44:32 +0300

    hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header';
+import heroData from './components/hero/hero.json';
+import Hero from './components/hero/Hero.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero props={heroData} />
     </div>
   );
 }
