00366d7504a6af4f448993892122d74f309bad9d
Koliadzhyn Yurii <Koliadjun@gmail.com>
2021-08-15 10:06:56 +0300

    header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+function Header(props) {
+    return (
+        <header>
+            <div></div>
+            <a href="tel:+78124016495">+7 (812) 401-64-95</a>
+            <button>Обратный звонок</button>
+        </header>
+    )
+}
+export default Header;
