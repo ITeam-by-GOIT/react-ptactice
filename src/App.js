b1075b8d2f19cb4afd4f325f32b98dce8878bbf7
Євгенія <76601332+Yevheniia87@users.noreply.github.com>
2021-08-17 16:36:59 +0300

    create component and css file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import heroData from './components/hero/hero.json';
 import Hero from './components/hero/Hero.jsx';
 import principlesData from './components/principles/principles.json';
 import Principles from './components/principles/Principles.jsx';
+import Contacts from './components/contacts/Contacts.jsx';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Hero props={heroData} />
       <Principles props={principlesData} />
+      <Contacts />
     </div>
   );
 }
