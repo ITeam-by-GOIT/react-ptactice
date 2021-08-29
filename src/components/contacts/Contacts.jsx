af88761c7ec5ff5ff2a1c4f2d8220ffbb71755ac
Євгенія <76601332+Yevheniia87@users.noreply.github.com>
2021-08-29 09:17:11 +0300

    added files

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,85 +1,41 @@
 import styles from '../contacts/contacts.module.css';
+import Forma from './Forma.jsx';
+import React, { Component } from 'react';
 
-function Contacts(props) {
-  return (
-    <section className={styles.contactsWrapper}>
-      <div className={styles.addressBlock}>
-        <h2 className={styles.mainTitle}>Работаем по всей России</h2>
-        <p className={styles.addressTitle}>Офис в Санкт-Петербурге:</p>
-        <ul className={styles.addressContainer}>
-          <li className={styles.addressItem}>
-            <a className={styles.phone} href="tel:+78124382635">
-              +7 (812) 438-26-35
-            </a>
-          </li>
-          <li className={styles.addressItem}>
-            <a href="mailto:info@skinfra.ru">info@skinfra.ru</a>
-          </li>
-          <li className={styles.addressItem}>
-            <a href="https://goo.gl/maps/PkxUZTVHjurX6CUi9">
-              195197, Санкт-Петербург, Кондратьевский пр., д. 15, к. 2, л. З,
-              офис 204 БЦ "Фернан Леже"
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h3 className={styles.feedback}>Свяжитесь с нами</h3>
-        <p className={styles.textWrapper}>
-          Задайте вопрос, пришлите коммерческое предложение или пригласите на
-          тендер
-        </p>
-        <form className={styles.formFeedback}>
-          <label htmlFor="" className={styles.formLabel}>
-            Представьтесь, пожалуйста*
-            <span className={styles.formWrapper}>
-              <input
-                type="text"
-                name="user-name"
-                className={styles.formInput}
-                placeholder="Ваше имя"
-              />
-            </span>
-          </label>
-          <label htmlFor="" className={styles.formLabel}>
-            Ваш телефон
-            <span className={styles.formWrapper}>
-              <input
-                type="text"
-                name="user-phone"
-                className={styles.formInput}
-                placeholder="+7 (___) ___-__-__"
-              />
-            </span>
-          </label>
-          <label htmlFor="" className={styles.formLabel}>
-            <span className={styles.formWrapper}>
-              <input
-                type="text"
-                name="user-file"
-                className={styles.formInput}
-                placeholder="Прикрепить файл"
-              />
-            </span>
-          </label>
-          <button type="submit" className={styles.buttonCall}>
-            Перезвоните мне
-          </button>
-          <span>
-            <input
-              type="checkbox"
-              name="user-policy"
-              id="check_policy"
-              className={styles.checkbox}
-            />
-            <label htmlFor="check_policy" className={styles.labelCheck}>
-              Я соглашаюсь с политикой конфиденциальности
-            </label>
-          </span>
-        </form>
-      </div>
-      <div className={styles.imageBlock}></div>
-    </section>
-  );
+export default class Contacts extends Component {
+  render() {
+    return (
+      <section className={styles.contactsWrapper}>
+        <div className={styles.addressBlock}>
+          <h2 className={styles.mainTitle}>Работаем по всей России</h2>
+          <p className={styles.addressTitle}>Офис в Санкт-Петербурге:</p>
+          <ul className={styles.addressContainer}>
+            <li className={styles.addressItem}>
+              <a className={styles.phone} href="tel:+78124382635">
+                +7 (812) 438-26-35
+              </a>
+            </li>
+            <li className={styles.addressItem}>
+              <a href="mailto:info@skinfra.ru">info@skinfra.ru</a>
+            </li>
+            <li className={styles.addressItem}>
+              <a href="https://goo.gl/maps/PkxUZTVHjurX6CUi9">
+                195197, Санкт-Петербург, Кондратьевский пр., д. 15, к. 2, л. З,
+                офис 204 БЦ "Фернан Леже"
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3 className={styles.feedback}>Свяжитесь с нами</h3>
+          <p className={styles.textWrapper}>
+            Задайте вопрос, пришлите коммерческое предложение или пригласите на
+            тендер
+          </p>
+          <Forma />
+        </div>
+        <div className={styles.imageBlock}></div>
+      </section>
+    );
+  }
 }
-export default Contacts;
